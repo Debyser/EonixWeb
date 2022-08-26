b62ff8a087b36453b8c1d7b7274b8275b1b97cdb
jdebyser <j.debyser@semlex.com>
2022-08-27 00:05:23 +0200

    removed unused files

--- WebApi/Models/ValidationMessageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EonixWebApi.Web.Models
-{
-    internal sealed class ValidationMessageViewModel
-    {
-        public string Description { get; set; }
-        public string PropertyName { get; set; }
-        public string Code { get; set; }
-    }
-}
